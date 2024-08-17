11eddd70aa1d1f74b36d7909e4bcec364239ca3a
lugerh <luige.rh@gmail.com>
2024-08-17 20:21:03 +0200

    Test VPS 8

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
     <h1>TaskTree Backend</h1>
-    <h2>Test 7 </h2>
+    <h2>Test 8 </h2>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
