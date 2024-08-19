0877a6f192162e34356b88560a2132895056f9c6
lugerh <luige.rh@gmail.com>
2024-08-19 23:54:32 +0200

    Clean

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 import express, { json } from 'express'
-import cors from 'cors';
+import cors from 'cors'
 
-import mongoose from 'mongoose';
-import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose'
+import cookieParser from 'cookie-parser'
 
 import projectRoutes from './routes/projectRoutes.js'
 import groupRoutes from './routes/groupRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import createAdminUser from './adminSetup.js'
 
-const { connect, connection } = mongoose;
-const app = express();
+const { connect, connection } = mongoose
+const app = express()
 
 // Conexión a MongoDB
-connect(process.env.MONGO_URI);
-const db = connection;
-db.on('error', console.error.bind(console, 'Error de conexión a MongoDB:'));
+connect(process.env.MONGO_URI)
+const db = connection
+db.on('error', console.error.bind(console, 'Error de conexión a MongoDB:'))
 db.once('open', () => {
   console.log('Conectado exitosamente a MongoDB')
   createAdminUser()
@@ -30,14 +30,13 @@ app.use(json())
 
 // Rutas
 app.get('/', (req, res) => {
-  res.send('Hola Mundo!');
-});
+  res.send('TaskTree backend WORKING!')
+})
 
+app.use('/api', [projectRoutes, groupRoutes, userRoutes])
 
-app.use('/api', [projectRoutes, groupRoutes, userRoutes]);
-
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 // const PORT = 8080;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto http://localhost:${PORT}`);
-});
+  console.log(`Servidor corriendo en el puerto http://localhost:${PORT}`)
+})
