606278c0a87b9de7a253638fffad4365ff41ae95
lugerh <luige.rh@gmail.com>
2024-08-19 23:45:06 +0200

    Clean & comment

--- models/Group.js
+++ models/Group.js
@@ -1,9 +1,18 @@
 // models/Group.js
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
 
 const groupSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
-});
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  members: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+})
 
-export default mongoose.model('Group', groupSchema);
+export default mongoose.model('Group', groupSchema)
--- models/Project.js
+++ models/Project.js
@@ -1,125 +1,124 @@
 // models/Project.js
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
 const checklistItemSchema = new mongoose.Schema({
-	completed: {
-		type: Boolean,
-		required: true,
-		default: false,
-	},
-	shortDescription: {
-		type: String,
-		required: true,
-	},
-	longDescription: {
-		type: String,
-		required: false,
-	},
-});
-
-// const textSchema = new mongoose.Schema({
-// 	shortDescription: {
-// 		type: String,
-// 		required: true,
-// 	},
-// 	longDescription: {
-// 		type: String,
-// 		required: false,
-// 	},
-// });
+  completed: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  shortDescription: {
+    // esto deberia generarse automaticamente para la interfaz, mostrar algo mas corto
+    type: String,
+    required: true,
+  },
+  longDescription: {
+    // esto es el input del usuario
+    type: String,
+    required: false,
+  },
+})
 
 const taskSchema = new mongoose.Schema({
-	title: {
-		type: String,
-		required: true,
-	},
-	description: {
-		type: String,
-		required: true,
-	},
-	text: {
-		shortDescription: {
-			type: String,
-			required: true,
-		},
-		longDescription: {
-			type: String,
-			required: false,
-		},
-	},
-	checklist: [checklistItemSchema],
-	status: {
-		type: String,
-		enum: ["Pending", "In Progress", "Completed"],
-		default: "Pending",
-	},
-	priority: {
-		type: String,
-		enum: ["High", "Medium", "Low"],
-		default: "Medium",
-	},
-	parent: {
-		type: mongoose.Schema.Types.ObjectId,
-		required: false,
-	},
-	hierarchyLevel: {
-		type: Number,
-		required: true,
-	},
-});
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  text: {
+    shortDescription: {
+      // esto deberia generarse automaticamente para la interfaz, mostrar algo mas corto
+      type: String,
+      required: true,
+    },
+    longDescription: {
+      // esto es el input del usuario
+      type: String,
+      required: true,
+    },
+  },
+  checklist: [checklistItemSchema],
+  status: {
+    type: String,
+    enum: ['Pending', 'In Progress', 'Completed'],
+    default: 'Pending',
+  },
+  priority: {
+    type: String,
+    enum: ['High', 'Medium', 'Low'],
+    default: 'Medium',
+  },
+  parent: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: false,
+  },
+  hierarchyLevel: {
+    type: Number,
+    required: true,
+  },
+})
 
 const objectiveSchema = new mongoose.Schema({
-	title: {
-		type: String,
-		required: true,
-	},
-	description: {
-		type: String,
-		required: true,
-	},
-	text: {
-		type: String,
-		required: true,
-	},
-	status: {
-		type: String,
-		enum: ["Pending", "In Progress", "Completed"],
-		default: "Pending",
-	},
-	priority: {
-		type: String,
-		enum: ["High", "Medium", "Low"],
-		default: "Medium",
-	},
-});
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['Pending', 'In Progress', 'Completed'],
+    default: 'Pending',
+  },
+  priority: {
+    type: String,
+    enum: ['High', 'Medium', 'Low'],
+    default: 'Medium',
+  },
+})
 
 const sharedUserSchema = new mongoose.Schema({
-	user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-	role: {
-		type: String,
-		enum: ["Read", "ReadWrite"],
-		required: true,
-	},
-});
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  role: {
+    type: String,
+    enum: ['Read', 'ReadWrite'],
+    required: true,
+  },
+})
 
 const projectSchema = new mongoose.Schema({
-	title: {
-		type: String,
-		required: true,
-	},
-	description: {
-		type: String,
-		required: true,
-	},
-	text: {
-		type: String,
-		required: true,
-	},
-	objectives: [objectiveSchema],
-	tasks: [taskSchema],
-	owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-	sharedGroup: { type: mongoose.Schema.Types.ObjectId, ref: "Group" },
-	sharedWith: [sharedUserSchema],
-});
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  objectives: [objectiveSchema],
+  tasks: [taskSchema],
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  sharedGroup: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Group',
+  },
+  sharedWith: [sharedUserSchema],
+})
 
-export default mongoose.model("Project", projectSchema);
+export default mongoose.model('Project', projectSchema)
