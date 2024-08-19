51141261116b1ed294b7b86bc49fb438a4a40d72
lugerh <luige.rh@gmail.com>
2024-08-20 01:45:30 +0200

    Save admin to mongo :)

--- adminSetup.js
+++ adminSetup.js
@@ -1,20 +1,30 @@
-import UserRepository from './repositories/UserRepository.js' // Asegúrate de importar tu repositorio de usuarios adecuadamente
+import User from './models/User.js' // Asegúrate de tener la ruta correcta a tu modelo de usuario
+import bcrypt from 'bcrypt'
 
 // Función para crear un usuario administrador si no existe
 async function createAdminUser() {
-  const adminExists = await UserRepository.findOne({ role: 'admin' })
-  if (!adminExists) {
-    UserRepository.create({
+  try {
+    const adminExists = await User.findOne({
       username: process.env.BACKEND_ADMIN_USER,
-      password: process.env.BACKEND_ADMIN_PASSWORD,
-      role: 'admin',
     })
-      .then(() => {
-        console.log('Administrador creado exitosamente')
+    if (!adminExists) {
+      const hashedPassword = await bcrypt.hash(
+        process.env.BACKEND_ADMIN_PASSWORD,
+        10
+      )
+      const adminUser = new User({
+        username: process.env.BACKEND_ADMIN_USER,
+        email: process.env.ADMIN_EMAIL,
+        password: hashedPassword,
+        role: 'Admin',
       })
-      .catch((err) => {
-        console.error('Error creando el usuario administrador:', err)
-      })
+      await adminUser.save()
+      console.log('Administrador creado exitosamente')
+    } else {
+      console.log('El administrador ya existe')
+    }
+  } catch (error) {
+    console.error('Error creando el usuario administrador:', error)
   }
 }
 
